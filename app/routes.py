b45575a9e5dd08e86555922ef00e59c1264b1b1a
Aman <amanjha434@gmail.com>
2025-05-12 13:42:24 +0530

    Co-authored-by: Akshit496 <Akshit496@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template,redirect
+from flask import Blueprint, render_template,redirect,request,url_for,session
 
 main = Blueprint('main',__name__)
 
@@ -8,6 +8,13 @@ def home():
     return render_template('homepage.html' )
 
 
-@main.route('/login')
+@main.route('/login',methods=['POST','GET'])
 def login():
+    if request.method == "POST":
+        user_name = request.form['username']
     return render_template('login.html')
+
+
+@main.route('/<usr>')
+def user(usr):
+    return f'Hi,{usr}'
