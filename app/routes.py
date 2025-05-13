b748443d610d80abd0905de4947d4a45500b481d
Aman <amanjha434@gmail.com>
2025-05-14 00:42:44 +0530

    registration form added with wtforms

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,30 @@
 from flask import Blueprint, render_template,redirect,request,url_for,session
+from wtforms import Form,StringField,validators,DateTimeField,PasswordField,SelectField
 
+
+class UserRegisterationForm(Form):
+    uName = StringField("Name: ",validators=[validators.InputRequired(),validators.length(min=2)])
+    uDob = DateTimeField("Date of Birth: ",validators=[validators.InputRequired()],format= '%Y-%m-%d')
+    uPass = PasswordField("Password: ",validators=[validators.InputRequired(),validators.length(min=8,max=150)])
+    uGender = SelectField("Gender: ",validators=[validators.InputRequired()],choices=[(1,"Male"),(2,"Female")])
 main = Blueprint('main',__name__)
 
 @main.route('/')
 @main.route('/home')
 def home():
-    return render_template('homepage.html' )
+    return render_template('homepage.html.j2' )
 
 
 @main.route('/login',methods=['POST','GET'])
 def login():
     if request.method == "POST":
         user_name = request.form['username']
-    return render_template('login.html')
+    return render_template('login.html.j2')
 
 
-@main.route('/<usr>')
-def user(usr):
-    return f'Hi,{usr}'
+@main.route('/register',methods=["GET","POST"])
+def register_user():
+    reg_form = UserRegisterationForm(request.form)
+    if request.method == "POST" and reg_form.validate():
+        print(reg_form.uName.data) # get hold of  data input in the field
+    return render_template('register.html.j2',render_form = reg_form)
